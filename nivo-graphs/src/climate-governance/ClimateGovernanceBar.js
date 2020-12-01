579b87c0bbf9a219ba9a7c68c80f9c2b50b4e682
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-12-01 09:31:53 -0800

    add bar adjustments

--- nivo-graphs/src/climate-governance/ClimateGovernanceBar.js
+++ nivo-graphs/src/climate-governance/ClimateGovernanceBar.js
@@ -6,68 +6,83 @@ const ClimateGovernanceBar = () => {
 
 
   const climateData =  climateGovernanceData.climateGovernanceData.barData;
+  console.log("climdt", climateData)
+
+  const data = [
+    {
+      question: 'Board level oversight',
+      'Oil & Gas': 9,
+      'Electric, Utility & Power Generators': 6
+    },
+    {
+      question: 'Climate-related performance',
+      'Oil & Gas': 12,
+      'Electric, Utility & Power Generators': 9
+    }
+]
 
   return (
   
   <ResponsiveBar
-      data={climateData}
-      keys={[ 'hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut' ]}
-      indexBy="country"
+      data={data}
+      keys={[ 'Oil & Gas', 'Electric, Utility & Power Generators']}
+      indexBy="question"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: 'linear' }}
       indexScale={{ type: 'band', round: true }}
       colors={{ scheme: 'nivo' }}
-      defs={[
-          {
-              id: 'dots',
-              type: 'patternDots',
-              background: 'inherit',
-              color: '#38bcb2',
-              size: 4,
-              padding: 1,
-              stagger: true
-          },
-          {
-              id: 'lines',
-              type: 'patternLines',
-              background: 'inherit',
-              color: '#eed312',
-              rotation: -45,
-              lineWidth: 6,
-              spacing: 10
-          }
-      ]}
-      fill={[
-          {
-              match: {
-                  id: 'fries'
-              },
-              id: 'dots'
-          },
-          {
-              match: {
-                  id: 'sandwich'
-              },
-              id: 'lines'
-          }
-      ]}
+      // defs={[
+      //     {
+      //         id: 'dots',
+      //         type: 'patternDots',
+      //         background: 'inherit',
+      //         color: '#38bcb2',
+      //         size: 4,
+      //         padding: 1,
+      //         stagger: true
+      //     },
+      //     {
+      //         id: 'lines',
+      //         type: 'patternLines',
+      //         background: 'inherit',
+      //         color: '#eed312',
+      //         rotation: -45,
+      //         lineWidth: 6,
+      //         spacing: 10
+      //     }
+      // ]}
+      // fill={[
+      //     {
+      //         match: {
+      //             id: 'fries'
+      //         },
+      //         id: 'dots'
+      //     },
+      //     {
+      //         match: {
+      //             id: 'sandwich'
+      //         },
+      //         id: 'lines'
+      //     }
+      // ]}
       borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+      borderRadius={52}
       axisTop={null}
       axisRight={null}
       axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
+          tickSize: 6,
+          tickPadding: 10,
           tickRotation: 0,
-          legend: 'country',
+          legend: 'Industry',
           legendPosition: 'middle',
-          legendOffset: 32
+          legendOffset: 40
       }}
       axisLeft={{
-          tickSize: 5,
+          tickSize: 10,
           tickPadding: 5,
           tickRotation: 0,
-          legend: 'food',
+          legend: 'Number of companies',
           legendPosition: 'middle',
           legendOffset: -40
       }}
@@ -80,11 +95,11 @@ const ClimateGovernanceBar = () => {
               anchor: 'bottom-right',
               direction: 'column',
               justify: false,
-              translateX: 120,
+              translateX: 50,
               translateY: 0,
-              itemsSpacing: 2,
+              itemsSpacing: 12,
               itemWidth: 100,
-              itemHeight: 20,
+              itemHeight: 60,
               itemDirection: 'left-to-right',
               itemOpacity: 0.85,
               symbolSize: 20,

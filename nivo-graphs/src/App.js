1c2772c75c7865da87854ecb0ed45483f864a4c6
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-12-01 12:52:00 -0800

    implement risks pie basics

--- nivo-graphs/src/App.js
+++ nivo-graphs/src/App.js
@@ -1,5 +1,6 @@
 import ClimateGovernance from './climate-governance/ClimateGovernance'
 import ClimateGovernanceBar from './climate-governance/ClimateGovernanceBar'
+import ClimateRisksOil from './climate-risks/ClimateRisksOil'
 import './index.css'
 
 function App() {
@@ -8,7 +9,15 @@ function App() {
       <h1>Climate Governance Charts</h1>
       <ClimateGovernance />
       <div>
+        <h2>Climate governance arrangements, by sector</h2>
         <ClimateGovernanceBar />
+      </div>
+      <div>
+        <h2>Risks identified as climate-related risks, by sector</h2>
+        <h3>Oil & Gas</h3>        
+        <ClimateRisksOil />
+        <h3>Electric, Utility & Energy Producers</h3>        
+        <ClimateRisksOil />
       </div>
     </div>
   );
---
+++ nivo-graphs/src/climate-risks/climate-risks-data.js
@@ -0,0 +1,49 @@
+const climateRisksData = {
+  pieData: [
+    {
+      "id": "Policy & Legal Risk",
+      "label": "Policy & Legal Risk",
+      "value": 12,
+      "color": "hsl(300, 70%, 50%)"
+    },
+    {
+      "id": "Technology Risk",
+      "label": "Technology Risk",
+      "value": 7,
+      "color": "hsl(193, 70%, 50%)"
+    },
+    {
+      "id": "Market Risk",
+      "label": "Market Risk",
+      "value": 9,
+      "color": "hsl(179, 70%, 50%)"
+    },
+    {
+      "id": "Reputation Risk",
+      "label": "Reputation Risk",
+      "value": 8,
+      "color": "hsl(55, 70%, 50%)"
+    },
+    {
+      "id": "Acute Risk",
+      "label": "Acute Risk",
+      "value": 9,
+      "color": "hsl(161, 70%, 50%)"
+    },
+    {
+      "id": "Chronic Risk",
+      "label": "Chronic Risk",
+      "value": 9,
+      "color": "hsl(161, 70%, 50%)"
+    },
+    {
+      "id": "Other Identified Risk",
+      "label": "Other Identified Risk",
+      "value": 3,
+      "color": "hsl(161, 70%, 50%)"
+    },
+
+  ]
+}
+
+module.exports = { climateRisksData }

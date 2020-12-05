5257b94c70f43d4999ace3c3d9f086a7299e7155
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-12-04 16:16:58 -0800

    implement chart helpers

--- nivo-graphs/src/data-helpers/barChartHelpers.js
+++ nivo-graphs/src/data-helpers/barChartHelpers.js
@@ -1,4 +1,6 @@
-module.exports = {
+const _ = require('lodash')
+
+const DataHelpers = {
 
   //Method to count up all 'response' fields (e.g. yes/no) given a 'columnHeader', returns the count
   countValues (columnHeader, response, dataArray) {
@@ -15,37 +17,75 @@ module.exports = {
   //Method that takes in the raw data, an xValues array of strings to be displayed along the x-axis
   //and a keysObject. The keys object takes as keys the names of the labels you want to give to the data, the values correspond 
   //to the column headers in the raw data (which we can feed into countValues function)
-  produceBarData (rawData, xValuesArray, dataLabels) {
+  produceBarData (lookupObject, response, rawData) {
     const retArr = [];
-    xValuesArray.map(label => {
-      retArr.push({xValue: label})
+    Object.keys(lookupObject).map(label => {
+      retArr.push({xValue: label});
+      console.log("retArr", retArr)
     });
+
+
     retArr.forEach(rowObject => {
-      for (let i=0; i < Object.keys(dataLabels).length; i++) {
-        rowObject[Object.keys(dataLabels)[i]] = this.countValues((Object.values(dataLabels)[i]), )
-      }
+      Object.values(lookupObject).map(unformattedLabel => {
+
+        const getKeyByValue = (object, value) => {
+          return Object.keys(object).find(key => object[key] === value);
+        }
+        
+        const formattedLabel = getKeyByValue(lookupObject, unformattedLabel)
+        rowObject[formattedLabel] = this.countValues(unformattedLabel, response, rawData)
+      })
+    })
+    return retArr;
+
+},
+
+  //Method that takes in a lookupObject where the keys are the formatted labels you want to show and the values are the 
+  //corresponding unformatted column labels in the table; a 'yes'/'no' response to query; and the data
+  //and returns a list of objects formatted for pie charts. 
+  producePieData (lookupObject, response, data) {
+    const retArr = [];
+    Object.keys(lookupObject).map( key => {
+      const retObj = {
+        id: key,
+        label: key,
+        value: this.countValues((lookupObject[key]), response, data)
+      };
+      const objCopy = _.cloneDeep(retObj);
+      retArr.push(objCopy);
+    });
+    return retArr;
   }
-
-
-
 };
 
 
+
+
+//////////// TEST DATA /////////////////
 let testData2 = [
   {
-    header: 'yes',
-    footer: 'yes'
+    unformatted_column_header_X: 'yes',
+    unformatted_column_header_Y: 'yes'
   },
   {
-    header: 'yes',
-    footer: 'no'
+    unformatted_column_header_X: 'yes',
+    unformatted_column_header_Y: 'no'
   },
   {
-    header: 'yes',
-    footer: 'no'
+    unformatted_column_header_X: 'yes',
+    unformatted_column_header_Y: 'no'
   },
   {
-    header: 'yes',
-    footer: 'no'
+    unformatted_column_header_X: 'yes',
+    unformatted_column_header_Y: 'no'
   }
 ]
+
+let refsObj = {
+  "Yes": "unformatted_column_header_X",
+  "No": "unformatted_column_header_Y"
+};
+
+let searchResponse = 'yes';
+
+console.log(DataHelpers.produceBarData(refsObj, searchResponse, testData2));
